1c833d7d7ee5b70af86ad575fcadc1bb80d41d43
Luis-Quito <jorgel.quito.lobo.2006@gmail>
2025-06-25 00:35:00 -0500

    Ejercicios 5 y 6

--- Solucion_Codigo/src/Problema6_Banco.java
+++ Solucion_Codigo/src/Problema6_Banco.java
@@ -12,5 +12,112 @@
  * @author Luis
  */
 public class Problema6_Banco {
-    
+    public static void main(String[] args) {
+        CuentaCheques cuenta1 = new CuentaCheques("C001", "Marco Herrera");
+        CuentaAhorros cuenta2 = new CuentaAhorros("C002", "Sofía Mendoza", 0.05);     
+        CuentaPlatino cuenta3 = new CuentaPlatino("C003", "Gabriel Salas");
+
+        cuenta1.depositar(800);
+        cuenta1.retirar(950); 
+
+        cuenta2.depositar(1500);
+        cuenta2.retirar(300);
+        cuenta2.calcularInteres(); 
+
+        cuenta3.depositar(2500);
+        cuenta3.retirar(700);
+        cuenta3.calcularInteres(); 
+
+        System.out.println(cuenta1);
+        System.out.println("\n" + cuenta2);
+        System.out.println("\n" + cuenta3);
+    }
+}
+class Cuenta {
+    public String codigoCuenta;
+    public String titular;
+    public double saldo;
+
+    public Cuenta() {}
+
+    public Cuenta(String codigoCuenta, String titular) {
+        this.codigoCuenta = codigoCuenta;
+        this.titular = titular;
+        this.saldo = 0.0;
+    }
+    public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+        }
+    }
+    public void retirar(double monto) {
+        if (monto > 0) {
+            saldo -= monto;
+        }
+    }
+    public double obtenerSaldo() {
+        return saldo;
+    }
+    public String toString() {
+        return "Cuenta N°: " + codigoCuenta 
+             + "\nTitular: " + titular 
+             + "\nSaldo actual: $" + saldo;
+    }
+}
+class CuentaAhorros extends Cuenta {
+    public double interesAnual;
+
+    public CuentaAhorros() {}
+
+    public CuentaAhorros(String codigoCuenta, String titular, double interesAnual) {
+        super(codigoCuenta, titular);
+        this.interesAnual = interesAnual;
+    }
+    public void calcularInteres() {
+        if (saldo > 0) {
+            double interes = saldo * interesAnual;
+            depositar(interes);
+        }
+    }
+    @Override
+    public void retirar(double monto) {
+        if (monto <= saldo) {
+            saldo -= monto;
+        }
+    }
+}
+class CuentaCheques extends Cuenta {
+    public CuentaCheques() {}
+
+    public CuentaCheques(String codigoCuenta, String titular) {
+        super(codigoCuenta, titular);
+    }
+    @Override
+    public void retirar(double monto) {
+        if (monto > 0) {
+            saldo -= monto;
+        }
+    }
+}
+class CuentaPlatino extends Cuenta {
+    public double interesFijo;
+
+    public CuentaPlatino() {}
+
+    public CuentaPlatino(String codigoCuenta, String titular) {
+        super(codigoCuenta, titular);
+        this.interesFijo = 0.10;
+    }
+    public void calcularInteres() {
+        if (saldo > 0) {
+            double interes = saldo * interesFijo;
+            depositar(interes);
+        }
+    }
+    @Override
+    public void retirar(double monto) {
+        if (monto > 0) {
+            saldo -= monto;
+        }
+    }
 }
